fc055b9005a285819928d1d9a1a03870e9e348a8
Hanane Ladj <hananeladj@gmail.com>
2021-05-07 02:02:21 -0700

    add JS Loop to the Progress Bar

--- js/app.js
+++ js/app.js
@@ -28,24 +28,61 @@ form.addEventListener("submit", function(event) {
 });
 
 
+// const skillsButton = document.querySelector('.skills-btn');
+// const percentage = document.querySelector('.percentage');
+
+// btn.addEventListener('click', () => {
+//   let progress = document.querySelector('progress').value;
+//   for ( let i = 0; i < progress.length; i++ ) {
+//     let counter = '';
+//     setInterval(() => {
+//       if (counter[i] === progress) {
+//         clearInterval();
+//       } else {
+//         counter++;
+//         percentage.innerHTML = `${counter}%`;
+//       }
+//     }, 20);
+//   }
+//   });
+  
+  
+
+const btn = document.querySelector('.skills-btn');
+const percentage = document.querySelector('.percentage');
+
+btn.addEventListener('click', () => {
+  let progress = document.querySelector('progress').value;
+  for ( let i = 0; i < progress; i++ ) {
+ 
+  setInterval(() => {
+    if (i === progress) {
+      clearInterval();
+    } else {
+      i++;
+      percentage.innerHTML = `${i}%`
+    }
+  }, 20)
+}
+})
 
 
 
-const progBar = document.querySelectorAll('#myProgress');
-for ( let i = 0; i < progBar.length; i++ ) {
-  let width = 90;
-  let id = setInterval(frame, 90);
-    function frame() {
+// const progBar = document.querySelectorAll('#myProgress');
+// for ( let i = 0; i < progBar.length; i++ ) {
+//   let width = 90;
+//   let id = setInterval(frame, 90);
+//     function frame() {
       
-        if (width >= 100) {
-          clearInterval(id);
-          i = 0;
-        } else {
-          width++;
-            progBar.style.width = width + "%";
-            progBar.innerHTML = width + "%";
-        }
-      }
+//         if (width >= 100) {
+//           clearInterval(id);
+//           i = 0;
+//         } else {
+//           width++;
+//             progBar.style.width = width + "%";
+//             progBar.innerHTML = width + "%";
+//         }
+//       }
       
     
-}
+// }
